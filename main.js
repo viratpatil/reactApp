7ada326829c133d3bf18946ea37d6134fd4843ab
viratpatil <viratpatil@gmail.com>
2017-09-17 20:39:25 +0530

    New Files

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search.jsx';
+//import Search from './components/Search.jsx';
+//import Movies from './components/Movies.jsx';
+import MainContainer from './components/MainContainer.jsx';
 
-ReactDOM.render(<Search />, document.getElementById('searchContainer'));
+//ReactDOM.render(<Search />, document.getElementById('searchContainer'));
+//ReactDOM.render(<Movies />, document.getElementById('searchResultContainer'));
+ReactDOM.render(<MainContainer />, document.getElementById('mainContainer'));
