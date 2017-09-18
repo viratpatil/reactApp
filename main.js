9774c6d31ac298668694ea7c9dbadfc5817ff07c
tonik1204 <las12041991@gmail.com>
2017-09-18 17:10:57 +0200

    reactapp

--- main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import Search from './components/Search.jsx';
-//import Movies from './components/Movies.jsx';
-import MainContainer from './components/MainContainer.jsx';
-
-//ReactDOM.render(<Search />, document.getElementById('searchContainer'));
-//ReactDOM.render(<Movies />, document.getElementById('searchResultContainer'));
-ReactDOM.render(<MainContainer />, document.getElementById('mainContainer'));
